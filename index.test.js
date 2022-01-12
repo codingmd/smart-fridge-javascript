3404fdf07eca75be688381377591e1e0093683da
kate Atherton <kateatherton222@gmail.com>
2022-01-12 19:03:45 +0000

    sets up test for checking if item added to fridge correctly

--- index.test.js
+++ index.test.js
@@ -1,7 +1,16 @@
-const { addNumbers, multiNumbers} = require('./index');
+const { addNumbers, multiNumbers, scanAddedItem } = require("./index");
+const Fridge = require("./fridge.js");
 
-describe('index', () => {
-    it('add numbers', () => {
-        expect(addNumbers(2, 2)).toBe(4);
-    })
-})
+describe("index", () => {
+  it("add numbers", () => {
+    expect(addNumbers(2, 2)).toBe(4);
+  });
+});
+
+describe("index", () => {
+  it("when 1 item added to fridge then fridge count is 1 ", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    Fridge.scanAddedItem(milk);
+    expect(Fridge.getItemCount()).toBe(1);
+  });
+});
