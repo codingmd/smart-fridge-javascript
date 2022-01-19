d238a6cb98f98f6058760db8be92aa6a772b7392
LauraMaddaluno <lauralondon.4@gmail.com>
2022-01-19 18:41:57 +0000

    test for timestamp

--- fridge.test.js
+++ fridge.test.js
@@ -2,10 +2,17 @@ const Item = require("./item.js");
 const Fridge = require("./fridge.js");
 
 describe("index", () => {
-	it("when 1 item added to fridge then fridge count is 1 ", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
-		fridge.scanAddedItem(milk);
-		expect(fridge.getItemCount()).toBe(1);
-	});
+  it("when 1 item added to fridge then fridge count is 1 ", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
+    fridge.scanAddedItem(milk);
+    expect(fridge.getItemCount()).toBe(1);
+  });
+  it("when 1 item is added timestamp is recorded", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
+    fridge.setCurrentDate("18/10/2021");
+    fridge.scanAddedItem(milk);
+    expect(fridge.setCurrentDate()).toBe("18/10/2021");
+  });
 });
