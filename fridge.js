21623e802228b4de9efc69dc1743a6c591118260
kate Atherton <kateatherton222@gmail.com>
2022-01-26 18:52:39 +0000

    adds items information to fridge

--- fridge.js
+++ fridge.js
@@ -1,21 +1,23 @@
 class Fridge {
-	itemCount = 0;
+  itemCount = 0;
+  items = [];
 
-	scanAddedItem(item) {
-		this.itemCount++;
-	}
+  scanAddedItem(item) {
+    this.itemCount++;
+    this.items.push(item);
+  }
 
-	getItemCount() {
-		return this.itemCount;
-	}
+  getItemCount() {
+    return this.itemCount;
+  }
 
-	setCurrentDate() {
-		const currentTime = new Date(
-			new Date().getTime() + 0 * 60 * 60 * 1000
-		).toLocaleTimeString();
-		console.log(currentTime)
-		return currentTime;
-	}
+  setCurrentDate() {
+    const currentTime = new Date(
+      new Date().getTime() + 0 * 60 * 60 * 1000
+    ).toLocaleTimeString();
+    console.log(currentTime);
+    return currentTime;
+  }
 }
 
 module.exports = Fridge;
--- fridge.test.js
+++ fridge.test.js
@@ -2,29 +2,28 @@ const Item = require("./item.js");
 const Fridge = require("./fridge.js");
 
 describe("index", () => {
-	it("when 1 item added to fridge then fridge count is 1 ", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
-		fridge.scanAddedItem(milk);
-		expect(fridge.getItemCount()).toBe(1);
-	});
-	it("when 2 items added to fridge then fridge count is 2", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const butter = new Item("butter", "21/10/21", "sealed");
-		const fridge = new Fridge();
-		fridge.scanAddedItem(milk);
+  it("when 1 item added to fridge then fridge count is 1 ", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
+    fridge.scanAddedItem(milk);
+    expect(fridge.getItemCount()).toBe(1);
+  });
+  it("when 2 items added to fridge then fridge count is 2", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const butter = new Item("butter", "21/10/21", "sealed");
+    const fridge = new Fridge();
+    fridge.scanAddedItem(milk);
 
-		fridge.scanAddedItem(butter);
-		expect(fridge.getItemCount()).toBe(2);
-	});
-	it("when 1 item is added timestamp is recorded", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
-		fridge.setCurrentDate("18/10/2021");
-		fridge.scanAddedItem(milk);
-		const currentTime = new Date(
-			new Date().getTime() + 0 * 60 * 60 * 1000
-		).toLocaleTimeString();
-		expect(fridge.setCurrentDate()).toBe(currentTime);
-	});
+    fridge.scanAddedItem(butter);
+    expect(fridge.getItemCount()).toBe(2);
+  });
+  it("when 1 item is added timestamp is recorded", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
+    fridge.scanAddedItem(milk);
+    const currentTime = new Date(
+      new Date().getTime() + 0 * 60 * 60 * 1000
+    ).toLocaleTimeString();
+    expect(fridge.setCurrentDate()).toBe(currentTime);
+  });
 });
