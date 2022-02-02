410bbb8763513dbb5d664604bd101d8d874a3470
Emma <48389529+emjogale@users.noreply.github.com>
2022-02-02 19:28:06 +0000

    add test for show display function

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # smart-fridge-javascript
 
-Here are some ideas for tests
+- [link to kata](https://katalyst.codurance.com/smart-fridge)
+  Here are some ideas for tests
 
 - adding items to the fridge (done)
 - adding a scan timestamp to items when added (done)
--- fridge.test.js
+++ fridge.test.js
@@ -2,44 +2,58 @@ const Item = require("./item.js");
 const Fridge = require("./fridge.js");
 
 describe("index", () => {
-  it("when 1 item added to fridge then fridge count is 1 ", () => {
-    const milk = new Item("milk", "21/10/21", "sealed");
-    const fridge = new Fridge();
+	it("when 1 item added to fridge then fridge count is 1 ", () => {
+		const milk = new Item("milk", "21/10/21", "sealed");
+		const fridge = new Fridge();
 
-    fridge.scanAddedItem(milk);
+		fridge.scanAddedItem(milk);
 
-    expect(fridge.getItemCount()).toBe(1);
-  });
+		expect(fridge.getItemCount()).toBe(1);
+	});
 
-  it("when 2 items added to fridge then fridge count is 2", () => {
-    const milk = new Item("milk", "21/10/21", "sealed");
-    const butter = new Item("butter", "21/10/21", "sealed");
-    const fridge = new Fridge();
+	it("when 2 items added to fridge then fridge count is 2", () => {
+		const milk = new Item("milk", "21/10/21", "sealed");
+		const butter = new Item("butter", "21/10/21", "sealed");
+		const fridge = new Fridge();
 
-    fridge.scanAddedItem(milk);
-    fridge.scanAddedItem(butter);
+		fridge.scanAddedItem(milk);
+		fridge.scanAddedItem(butter);
 
-    expect(fridge.getItemCount()).toBe(2);
-  });
+		expect(fridge.getItemCount()).toBe(2);
+	});
 
-  it("when 1 item is added timestamp is recorded", () => {
-    const milk = new Item("milk", "21/10/21", "sealed");
-    const fridge = new Fridge();
+	it("when 1 item is added timestamp is recorded", () => {
+		const milk = new Item("milk", "21/10/21", "sealed");
+		const fridge = new Fridge();
 
-    fridge.scanAddedItem(milk);
+		fridge.scanAddedItem(milk);
 
-    expect(milk.scannedTime).not.toBe(null);
-  });
+		expect(milk.scannedTime).not.toBe(null);
+	});
 
-  it("when 1 item is removed from the fridge, the item count is reduced by 1", () => {
-    const milk = new Item("milk", "21/10/21", "sealed");
-    const butter = new Item("butter", "21/10/21", "sealed");
-    const fridge = new Fridge();
+	it("when 1 item is removed from the fridge, the item count is reduced by 1", () => {
+		const milk = new Item("milk", "21/10/21", "sealed");
+		const butter = new Item("butter", "21/10/21", "sealed");
+		const fridge = new Fridge();
 
-    fridge.scanAddedItem(milk);
-    fridge.scanAddedItem(butter);
-    fridge.removeItem(milk);
+		fridge.scanAddedItem(milk);
+		fridge.scanAddedItem(butter);
+		fridge.removeItem(milk);
 
-    expect(fridge.getItemCount()).toBe(1);
-  });
+		expect(fridge.getItemCount()).toBe(1);
+	});
+
+	it("displays items in fridge", () => {
+		const milk = new Item("milk", "21/10/21", "sealed");
+		const butter = new Item("butter", "21/10/21", "sealed");
+		const fridge = new Fridge();
+
+		fridge.scanAddedItem(milk);
+		fridge.scanAddedItem(butter);
+
+		expect(fridge.showDisplay()).toBe([
+			{ name: "milk", expiry: "21/10/21", condition: "sealed" },
+			{ name: "butter", expiry: "21/10/21", condition: "sealed" },
+		]);
+	});
 });
