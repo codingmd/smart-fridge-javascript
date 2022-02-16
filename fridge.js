79e823b71cd39b4f2905ee8fa3bb9415fd096140
LauraMaddaluno <lauralondon.4@gmail.com>
2022-02-16 18:52:02 +0000

    refactor remove items from fridge

--- fridge.js
+++ fridge.js
@@ -1,33 +1,33 @@
 class Fridge {
-	itemCount = 0;
-	items = [];
+  itemCount = 0;
+  items = [];
 
-	scanAddedItem(item) {
-		this.itemCount++;
-		item.scannedTime = this.setCurrentDate();
-		this.items.push(item);
-	}
+  scanAddedItem(item) {
+    this.itemCount++;
+    item.scannedTime = this.setCurrentDate();
+    this.items.push(item);
+  }
 
-	isItemInFridge(item) {
-		return this.items.includes(item);
-	}
+  isItemInFridge(item) {
+    return this.items.includes(item);
+  }
 
-	getItemCount() {
-		return this.itemCount;
-	}
+  getItemCount() {
+    return this.itemCount;
+  }
 
-	setCurrentDate() {
-		const currentTime = new Date().toLocaleTimeString();
-		console.log(currentTime);
-		return currentTime;
-	}
+  setCurrentDate() {
+    const currentTime = new Date().toLocaleTimeString();
+    console.log(currentTime);
+    return currentTime;
+  }
 
-	removeItemFromFridge(item) {
-		if (this.isItemInFridge(item)) {
-			this.itemCount--;
-			return true;
-		}
-		return false;
-	}
+  removeItemFromFridge(item) {
+    if (this.isItemInFridge(item)) {
+      this.itemCount--;
+      // return true;
+    }
+    // return false;
+  }
 }
 module.exports = Fridge;
--- fridge.test.js
+++ fridge.test.js
@@ -2,90 +2,93 @@ const Item = require("./item.js");
 const Fridge = require("./fridge.js");
 
 describe("index", () => {
-	it("when 1 item added to fridge then fridge count is 1 ", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
+  it("when 1 item added to fridge then fridge count is 1 ", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
 
-		fridge.scanAddedItem(milk);
+    fridge.scanAddedItem(milk);
 
-		expect(fridge.getItemCount()).toBe(1);
-	});
+    expect(fridge.getItemCount()).toBe(1);
+  });
 
-	it("when 2 items added to fridge then fridge count is 2", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const butter = new Item("butter", "21/10/21", "sealed");
-		const fridge = new Fridge();
+  it("when 2 items added to fridge then fridge count is 2", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const butter = new Item("butter", "21/10/21", "sealed");
+    const fridge = new Fridge();
 
-		fridge.scanAddedItem(milk);
-		fridge.scanAddedItem(butter);
+    fridge.scanAddedItem(milk);
+    fridge.scanAddedItem(butter);
 
-		expect(fridge.getItemCount()).toBe(2);
-	});
+    expect(fridge.getItemCount()).toBe(2);
+  });
 
-	it("when 1 item is added timestamp is recorded", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
+  it("when 1 item is added timestamp is recorded", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
 
-		fridge.scanAddedItem(milk);
+    fridge.scanAddedItem(milk);
 
-		expect(milk.scannedTime).not.toBe(null);
-	});
+    expect(milk.scannedTime).not.toBe(null);
+  });
 
-	it("when 1 item is removed from the fridge, the item count is reduced by 1", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const butter = new Item("butter", "21/10/21", "sealed");
-		const fridge = new Fridge();
+  it("when 1 item is removed from the fridge, the item count is reduced by 1", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const butter = new Item("butter", "21/10/21", "sealed");
+    const fridge = new Fridge();
 
-		fridge.scanAddedItem(milk);
-		fridge.scanAddedItem(butter);
-		fridge.removeItemFromFridge(milk);
+    fridge.scanAddedItem(milk);
+    fridge.scanAddedItem(butter);
+    fridge.removeItemFromFridge(milk);
 
-		expect(fridge.getItemCount()).toBe(1);
-	});
+    expect(fridge.getItemCount()).toBe(1);
+  });
 
-	it("when the fridge is empty the count will not go below 0", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
+  it("when the fridge is empty the count will not go below 0", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
 
-		fridge.removeItemFromFridge(milk);
-		expect(fridge.getItemCount()).toBe(0);
-	});
+    fridge.removeItemFromFridge(milk);
+    expect(fridge.getItemCount()).toBe(0);
+  });
+  it("only items in fridge can be removed from fridge", () => {
+    const fridge = new Fridge();
+    const itemCount = fridge.getItemCount();
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const butter = new Item("butter", "21/10/21", "sealed");
+    fridge.scanAddedItem(milk);
+    // fridge.scanAddedItem(butter);
+    fridge.removeItemFromFridge(butter);
+    expect(fridge.getItemCount()).toBe(1);
+  });
 
-	it("only items in fridge can be removed from fridge", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
-		fridge.scanAddedItem(milk);
-		expect(fridge.removeItemFromFridge(milk)).toBe(true);
-	});
+  //   it(" items not in fridge can't be removed from fridge", () => {
+  //     const milk = new Item("milk", "21/10/21", "sealed");
+  //     const butter = new Item("butter", "21/10/21", "sealed");
+  //     const fridge = new Fridge();
+  //     fridge.scanAddedItem(milk);
 
-	it(" items not in fridge can't be removed from fridge", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const butter = new Item("butter", "21/10/21", "sealed");
-		const fridge = new Fridge();
-		fridge.scanAddedItem(milk);
+  //     expect(fridge.removeItemFromFridge(butter)).toBe(false);
+  //   });
 
-		expect(fridge.removeItemFromFridge(butter)).toBe(false);
-	});
+  it("verify item has been added to fridge", () => {
+    const milk = new Item("milk", "21/10/21", "sealed");
+    const fridge = new Fridge();
 
-	it("verify item has been added to fridge", () => {
-		const milk = new Item("milk", "21/10/21", "sealed");
-		const fridge = new Fridge();
+    fridge.scanAddedItem(milk);
+    expect(fridge.isItemInFridge(milk)).toBe(true);
+  });
 
-		fridge.scanAddedItem(milk);
-		expect(fridge.isItemInFridge(milk)).toBe(true);
-	});
+  // it("displays items in fridge", () => {
+  // 	const milk = new Item("milk", "21/10/21", "sealed");
+  // 	const butter = new Item("butter", "21/10/21", "sealed");
+  // 	const fridge = new Fridge();
 
-	// it("displays items in fridge", () => {
-	// 	const milk = new Item("milk", "21/10/21", "sealed");
-	// 	const butter = new Item("butter", "21/10/21", "sealed");
-	// 	const fridge = new Fridge();
+  // 	fridge.scanAddedItem(milk);
+  // 	fridge.scanAddedItem(butter);
 
-	// 	fridge.scanAddedItem(milk);
-	// 	fridge.scanAddedItem(butter);
-
-	// 	expect(fridge.showDisplay()).toBe([
-	// 		{ name: "milk", expiry: "21/10/21", condition: "sealed" },
-	// 		{ name: "butter", expiry: "21/10/21", condition: "sealed" },
-	// 	]);
-	// });
+  // 	expect(fridge.showDisplay()).toBe([
+  // 		{ name: "milk", expiry: "21/10/21", condition: "sealed" },
+  // 		{ name: "butter", expiry: "21/10/21", condition: "sealed" },
+  // 	]);
+  // });
 });
