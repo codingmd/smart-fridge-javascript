9b286b4575e5280075a9eb7cfa4f30f18816913d
Emma Gale <emmajgale@hotmail.com>
2022-03-02 18:32:26 +0000

    start to reformat date into year month day

--- item.js
+++ item.js
@@ -1,17 +1,21 @@
 class Item {
-  constructor(name, expiry, condition) {
-    this.name = name;
-    this.expiry = setExpiryDate(expiry);
-    this.condition = condition;
-    this.scannedTime = null;
-  }
+	constructor(name, expiry, condition) {
+		this.name = name;
+		this.expiry = this.setExpiryDate(expiry);
+		this.condition = condition;
+		this.scannedTime = null;
+	}
 
-  setExpiryDate(expiry) {
-    //expiry = "day/month/year"
-    const expiryDate = new Date(expiry);
-    console.log(expiryDate);
-    return expiryDate;
-  }
+	setExpiryDate(expiry) {
+		//expiry = "day/month/year"
+		//slice(6, 8), slice(3, 5), slice(0, 2)
+		const day = expiry.slice(0, 2);
+		const month = expiry.slice(3, 5);
+		const year = "20" + expiry.slice(6, 8);
+		const expiryDate = new Date(expiry.slice(6, 8));
+		console.log(day, month, year);
+		return expiryDate;
+	}
 }
 
 module.exports = Item;
