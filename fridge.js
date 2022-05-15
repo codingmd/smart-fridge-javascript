df1450441616c9b9aa8b74603b081ee407b86c04
codingmd <codepro.md@gmail.com>
2022-05-15 01:37:09 -0700

    Added fridge app functionality—html, css, and js scripts—to display fridge and item selection

--- fridge.js
+++ fridge.js
@@ -1,3 +1,34 @@
+const Item = require("./item.js");
+var modal = document.getElementById("itemsModal");
+var addItemsBtn = document.getElementById("addItems");
+var span = document.getElementsByClassName("close")[0];
+
+addItemsBtn.onclick = function () {
+	modal.style.display = "block";
+}
+
+span.onclick = function () {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal and all items selected, close it
+window.onclick = function (event) {
+	if (event.target == modal && counter > 0) {
+		modal.style.display = "none";
+		addItemsBtn.display = "none";
+	}
+}
+
+function checkPosition(id) {
+	if (counter < 2) {
+		if (!this.items.includes(this.availableItems[id])) {
+			this.items.push(this.availableItems[id])
+			counter = counter + 1;
+			document.getElementById("counter").innerHTML = counter;
+		}
+	}
+}
+
 class Fridge {
 	itemCount = 0;
 	items = [];
@@ -6,6 +37,20 @@ class Fridge {
 	inDateItemsArray = [];
 	formattedDisplayArray = [];
 	currentDate = new Date();
+	itemsSelected = [];
+	counter = 0;
+	availableItems = initializeFridgeItems();
+
+	initializeFridgeItems() {
+		return [
+			{ 0: new Item("milk", "21/05/22", "opened") },
+			{ 1: new Item("butter", "21/06/22", "sealed") },
+			{ 2: new Item("yoghurt", "21/04/22", "sealed") },
+			{ 3: new Item("cheese", "2/07/22", "sealed") },
+			{ 4: new Item("tofu", "2/06/22", "sealed") },
+			{ 5: new Item("apple", "2/06/22", "sealed") }
+		];
+	}
 
 	signalDoorOpened() {
 		this.fridgeDoorOpened = true;
@@ -93,9 +138,9 @@ class Fridge {
 			}
 			this.formattedDisplayArray.push(
 				this.inDateItemsArray[i].name +
-					": " +
-					this.inDateItemsArray[i].daysLeftToEat +
-					daysremaining
+				": " +
+				this.inDateItemsArray[i].daysLeftToEat +
+				daysremaining
 			);
 		}
 	}
@@ -116,5 +161,6 @@ class Fridge {
 		this.currentDate = this.currentDate.setDate(this.currentDate.getDate() + 1);
 	}
 }
+
 
 module.exports = Fridge;
